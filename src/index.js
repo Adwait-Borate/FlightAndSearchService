55e34e6f473a9dd92cb953be80000beafe765f84
Adwait Borate <adwaitborate@gmail.com>
2024-06-22 18:47:50 +0530

    Added Body-Parser in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express=require("express");
-require('dotenv').config()
+const bodyParser=require("body-parser");
 
 const PORT=require('./config/serverConfig');
 const setupAndStartServer=async()=>{
+
     //create the express object
     const app=express();
-    const PORT=3000;
+    
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({"extended":true}));
 
     app.listen(3000,()=>{
         console.log(`Server started at ${PORT}`);
