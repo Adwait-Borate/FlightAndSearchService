2a9c151c50c70f083637c0f8f654a33f07c80fec
Adwait Borate <adwaitborate@gmail.com>
2024-06-23 12:11:07 +0530

    Added city-service.js in service folder

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,27 +1,58 @@
-const City=require('../models/index')   //in index file it will return all the models that you create,so need to import models individually.
+const { City } = require('../models/index');   //in index file it will return all the models that you create,so need to import models individually.
 
 class CityRepository {
-    async createCity({name}) {
-      try {
-        const city = await City.create({ name });
-        return city;
-      } catch (error) {
-        console.error('Error creating city:', error);
-        throw error;
-      }
-    }
-  
-    async deleteCityById(id) {
-     try{
-         await City.destroy({
-            where: {
-                id:cityId
-            }
-         });
-     }catch(error){
-        throw{error};
-     }
+  async createCity({ name }) {
+    try {
+      const city = await City.create({ name });
+      return city;
+    } catch (error) {
+      console.error('Error creating city:', error);
+      throw error;
     }
   }
+
+  async deleteCityById(cityId) {
+    try {
+      await City.destroy({
+        where: {
+          id: cityId
+        }
+      });
+      return true;
+    } catch (error) {
+      console.error('Error deleting city:', error);
+      throw error;
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await City.findByPk(cityId);  
+      return city;  // Return the result of the findByPk operation
+    } catch (error) {
+      console.error('Something went wrong in the repository layer:', error);
+      throw error;
+    }
+  }
+
+
+  async updateCity(cityId, data) {
+    try {
+      const city = await City.update(data, {
+        where: {
+             id: cityId }  // Correctly specifying the condition in the where clause
+      });
+      return city;
+    } catch (error) {
+      console.error('Something went wrong in the repository layer:', error);
+      throw error;
+    }
+  }
+
+}
+
+
+
   
-  module.exports =  CityRepository();
+
+module.exports =  CityRepository();  // Correctly export an instance of CityRepository
---
+++ src/repository/index.js
@@ -0,0 +1,7 @@
+//instead of exporting every repository seperately, we will instead export an object of all repositories.
+
+const cityRepository = require("./city-repository");
+
+module.exports={
+    cityRepository: require('./city-repository')
+}
