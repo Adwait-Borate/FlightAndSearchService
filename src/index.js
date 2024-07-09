811707f9976ac3c196ccb1c9ef4ff85987fb9488
Adwait Borate <adwaitborate@gmail.com>
2024-07-09 23:50:00 +0530

    Database sync

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const bodyParser=require("body-parser");
 const {PORT}=require('./config/serverConfig');
 const ApiRoutes=require('./routes/index');
 const setupAndStartServer=async()=>{
-
+const db=require('./models/index');
     //create the express object
     const app=express();
     
@@ -12,6 +12,11 @@ const setupAndStartServer=async()=>{
     app.use('/api', ApiRoutes);                        // api/v1/city
     app.listen(PORT,()=>{
         console.log(`Server started at ${PORT}`);
+
+        // Synchronize the database as with we are associating tables with each other
+        if(process.env.SYNC_DB){
+            db.sequelize.sync({alter:true});
+        }
        // console.log(process.env) // remove this after you've confirmed it is working
     });
 }
